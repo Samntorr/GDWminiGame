317fd2de82be644f4353bc0fa0f856f0241c8de9
Sam <storres6@student.fitchburgstate.edu>
2022-02-13 17:00:13 -0500

    level desgin

--- Minigame1/Assets/Scritps/PlayerController.cs
+++ Minigame1/Assets/Scritps/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float roadSpeed = 5f;
+    public float curbSpeed = 4f;
+
+    //range for bounds
+    public float xRange = 40f;
+    public float yRange = 90f;
 
     public Rigidbody2D rb;
 
@@ -17,6 +23,23 @@ public class PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        //create bounds for the player
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x, yRange);
+        }
     }
 
     private void FixedUpdate()
@@ -27,27 +50,31 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //change speed to be fast on the road
         if (collision.CompareTag("Road"))
         {
-            moveSpeed = moveSpeed + 5;
+            moveSpeed = moveSpeed + roadSpeed;
         }
 
+        //change speed to be slower on the curb
         if (collision.CompareTag("Curb"))
         {
-            moveSpeed = moveSpeed - 3;
+            moveSpeed = moveSpeed - curbSpeed;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //bringing back to normal
         if (collision.CompareTag("Road"))
         {
-            moveSpeed = moveSpeed - 5;
+            moveSpeed = moveSpeed - roadSpeed;
         }
 
+        //bringing speed back to normal
         if (collision.CompareTag("Curb"))
         {
-            moveSpeed = moveSpeed + 3;
+            moveSpeed = moveSpeed + curbSpeed;
         }
     }
 }
